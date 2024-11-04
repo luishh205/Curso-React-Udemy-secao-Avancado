e7b537c2285bb89f860080f68042fecea97d1920
Luis Henrique Ramos Soares <89323852+luishh205@users.noreply.github.com>
2024-11-04 08:39:20 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import FistComponent from './Components/FistComponent';
+import City from './Assets/City.jpeg';
 
 function App() {
   return (
     <div className="App">
       <div>
          <h1>Começãndo a Seção Avançado</h1>
+         <div>
+          <img src="/Paisagem.jpeg" alt="paisagem" />
+         </div>
+         <div>
+          <img src={City} alt="Cidade" />
+         </div>
       </div>
       <FistComponent/>
     </div>
